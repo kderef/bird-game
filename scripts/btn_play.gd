6ba5b8f705e7f6ec7fd5218075712dd3d3bf676c
Fe-Sc <fschintz@gmail.com>
2024-05-28 18:37:03 +0200

    betere olaf en litte muiskes

--- scripts/btn_play.gd
+++ scripts/btn_play.gd
@@ -5,7 +5,7 @@ var tree
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	tree = get_tree()
-	pressed.connect(func(): tree.change_scene_to_file("res://game.tscn"))
+	pressed.connect(func(): tree.change_scene_to_file("res://scenes/Game.tscn"))
 	# TODO: add transition
 	pass # Replace with function body.
 
