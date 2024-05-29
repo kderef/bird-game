f090e504fe49fd7e7003bb892d84ffe8572e1ddd
Fe-Sc <fschintz@gmail.com>
2024-05-29 11:30:46 +0200

    olaf heeft nu een idle animation!!!

--- scenes/CharacterBody2D2.gd
+++ scenes/CharacterBody2D2.gd
@@ -3,14 +3,18 @@ extends CharacterBody2D
 var speed = 60  # speed in pixels/sec
 
 func _physics_process(delta):
+	if velocity == Vector2(0, 0):
+		$Sprite2D/olafanim.play("idle-loop")
+	if velocity > Vector2(0,0):
+		$Sprite2D/olafanim.stop()
 	var direction = Input.get_vector("left", "right", "up", "down")
 	velocity = direction * speed
 	if Input.is_action_pressed("left"):
-		$Sprite2D.flip_h = false
+		$Sprite2D.flip_h = true
 		$Sprite2D.flip_v = false
 
 	if Input.is_action_just_pressed("right"):
-		$Sprite2D.flip_h = true
+		$Sprite2D.flip_h = false
 		$Sprite2D.flip_v = false
 
 	if Input.is_action_just_pressed("run"):
@@ -18,7 +22,8 @@ func _physics_process(delta):
 	if Input.is_action_just_released("run"):
 		speed = 60
 
+		
+
 
 	move_and_slide()
 	
-
