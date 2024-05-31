37c91390ebe74241c432997b248b56b7289d5cbc
Fe-Sc <fschintz@gmail.com>
2024-05-31 20:02:26 +0200

    collision house;

--- scenes/Sprite2D.gd
+++ scenes/Sprite2D.gd
@@ -1,15 +1,13 @@
-extends Button
+extends Sprite2D
 
-var tree
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	tree = get_tree()
-	pressed.connect(func(): tree.change_scene_to_file("res://scenes/house.tscn"))
-	# TODO: add transition
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	var walk = true
+	$vrouw/AnimationPlayer.play("girlwalk")
 	pass
