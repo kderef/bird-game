72ec96537d6024d1bd6b448395ed5c91675ca9f4
kvoid <kianheitkamp@outlook.com>
2024-06-12 13:47:34 +0200

    fix player speed to deltatime

--- player.gd
+++ player.gd
@@ -1,11 +1,11 @@
 extends CharacterBody2D
 
-var speed = 60  # speed in pixels/sec
+var speed = 4000  # speed, compensated by delta time.
 
 func _physics_process(delta):
 	#movement
 	var direction = Input.get_vector("left", "right", "up", "down")
-	velocity = direction * speed
+	velocity = (direction * speed) * delta
 	if Input.is_action_pressed("left"):
 		$player_sprite.flip_h = true
 		$player_sprite.flip_v = false
