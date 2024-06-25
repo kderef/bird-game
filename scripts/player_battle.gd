436d128cedc7e667de3eb0f20bf3866ec249efdd
kvoid <kianheitkamp@outlook.com>
2024-06-25 09:50:19 +0200

    add olaf attack animations skibidi ohio rizzler livvie dunne baby gronk moment 69 420 ohio gedagido roblox bobux lankybox moment

--- scripts/player_battle.gd
+++ scripts/player_battle.gd
@@ -1,25 +1,38 @@
 extends CharacterBody2D
 
 enum PlayerState {
-    Idle, Act
+	Idle, Attack
 }
 
-var player_state = PlayerState.Idle
+var player_state = PlayerState.Attack
+var attack_index = 1
 
-func play_animation():
-    match player_state:
-        PlayerState.Idle:
-            $player_sprite_idle.visible = true
-            $player_sprite_attack.visible = false
-            $player_sprite_idle/idleanim.play("idleanim")
-        PlayerState.Act:
-            $player_sprite_idle.visible = false
-            $player_sprite_attack.visible = true
-            # TODO: add attack animation (NOTE: first: fix spritesheet)
-            #$player_sprite_attack/attackanim.play("attackanim")
+func player_attack():
+	$player_sprite_idle.visible = false
+	$player_sprite_attack.visible = true
+	
+	if attack_index > 3:
+		attack_index = 1
+	else:
+		attack_index += 1
+
+	if attack_index == 1:
+		$player_sprite_attack/attackanim.play("attackanim1")
+	elif attack_index == 2:
+		$player_sprite_attack/attackanim.play("attackanim2")
+	else:
+		$player_sprite_attack/attackanim.play("attackanim3")
+
+func player_idle():
+	$player_sprite_idle.visible = true
+	$player_sprite_attack.visible = false
+	$player_sprite_idle/idleanim.play("idleanim")
 
 func _ready():
-    play_animation()
+	player_idle()
 
 func _process(delta):
-    play_animation()
+	if Input.is_action_just_pressed("right"):
+		player_attack()
+	if not $player_sprite_attack/attackanim.is_playing():
+		player_idle()
